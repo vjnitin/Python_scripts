ba07de6eec2f7e9d2dd27ff5a928969592b23b9a
vjnitin <n.nitin@op.iitg.ac.in>
2024-10-11 08:18:24 +0530

    Update Prime_checker.py

--- Prime_checker.py
+++ Prime_checker.py
@@ -3,20 +3,13 @@ while True:
     try:
         usr_input = int(input("Enter an integer: "))
         if usr_input > 1:
-            is_prime = True
             for i in range(2, usr_input):
                 if usr_input % i == 0:
-                    is_prime = False
-                    break
-            if is_prime:
-                print("The number is prime.")
-            else:
-                print("The number is not prime.")
+                    print("The number is not prime.")
+                else:
+                    print("The number is prime.")
         else:
-            if usr_input < 0:
-                print("Enter only the positive integer.")
-            else:
-                print("The number is not prime.")
+            print("Enter only the positive integer.")
     except:
         print("The program ended by entering other than int object.")
         break
